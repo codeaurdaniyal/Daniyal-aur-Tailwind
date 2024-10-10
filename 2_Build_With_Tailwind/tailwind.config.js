1395afbfcacd837e6e5cdfe59e6b1cccaf20e7d3
codeaurdaniyal <daniyalsaleem686@gmail.com>
2024-10-10 15:24:07 -0700

    Add New Content & Delete some images

--- 2_Build_With_Tailwind/tailwind.config.js
+++ 2_Build_With_Tailwind/tailwind.config.js
@@ -1,25 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/**/*.html"],
+  content: ['./dist/**/*.html'],
   theme: {
     extend: {
       /* <-- Custom Styling --> */
         fontFamily: {
           poppin: ['Poppins', 'sans-serif'],
         },
+        backgroundColor:{
+          'black-l-1': 'rgb(9, 9, 9)',
+          'black-l-2': 'rgb(9, 10, 10)',
+        },
         height:{
-          fit: "fit-content",
+          fit: 'fit-content',
         },
         width:{
-          fit: "fit-content"
+          fit: 'fit-content'
         },
         boxShadow:{
           'black-100': '0 2px 4px rgba(0, 0, 0, .7)', 
         },
-        keyframe:{
-          moveup:{
-            "0%":{  },
+        keyframes:{
+          movedown:{
+            '0%':{ transform: 'translateY(0)'},
+            '100%':{ transform: 'translateY(-936px)'},
           },
+          moveup:{
+            '0%':{ transform: 'translateY(0)'},
+            '100%':{ transform: 'translateY(936px)'},
+          },
+          moveright:{
+            '0%':{ left: '-90%' },
+            '100%':{ left: '300%' }
+          },
+        },
+        animation:{
+          movedown: 'movedown 29s linear infinite',
+          moveup: 'moveup 29s linear infinite',
+          moveright: 'moveright 5s linear infinite',
         },
         aspectRatio:{
           '1/2': '1/2',
@@ -30,11 +48,15 @@ module.exports = {
   plugins: [
     function ({ addUtilities }) {
       addUtilities({
-        '.scrollbar-none': { 'scrollbar-width': 'none' },
-        '.shadow-before': { '&::before': { content: "''", position: 'absolute', top: '-9px', left: '0', right: '0', height: '30px', width: '100%', 'box-shadow': 'inset 0 23px 18px rgba(9, 9, 11)'},
-        },
-        '.shadow-after': { '&::after': { content: "''", position: 'absolute', bottom: '-7px', left: '0', right: '0', height: '30px', width: '100%', 'box-shadow': 'inset 0 -23px 18px rgba(9, 9, 11)'},
-        },
+        // Shadow::Before & ::After
+        '.shadow-before': { '&::before': { content: '""', 'box-shadow': 'inset 0 23px 18px rgba(9, 9, 11)', height: '30px', width: '100%', position: 'absolute', top: '-7px', left: '0', right: '0', 'z-index': '3'}},
+        '.shadow-after': { '&::after': { content: '""', 'box-shadow': 'inset 0 -23px 18px rgba(9, 9, 11)', height: '30px', width: '100%', position: 'absolute', bottom: '-7px', left: '0', right: '0', 'z-index': '3'}},
+        // Shadow::Before & ::After 
+
+        // Move Div::Before & ::After 
+        '.moveable-div-before': { '&::before': { content: '""', 'background-color': 'rgb(24 24 27)', height: '84%', width: '97%', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', 'border-radius': '1.5rem', 'z-index': '2'}},
+        '.moveable-div-after': { '&::after': { content: '""', 'background-color': 'rgb(244, 244, 244)', height: '100%', width: '110%', position: 'absolute', top: '-60%', transform: 'rotate(-30deg) translate(-50%, -50%)', 'border-radius': '1.5rem', 'z-index': '1'}},
+        // Move Div::Before & ::After 
       });
     },
   ],
